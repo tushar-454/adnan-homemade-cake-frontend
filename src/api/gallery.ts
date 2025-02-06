c86da2ad4f3b0b7f58d64d76b832ffb44e6dc0b2
tushar-454 <imtushar454@gmail.com>
2025-02-06 23:45:24 +0600

    show all gallery item table in dashboard and add gallery item deletion functionality with confirmation dialog and error handling

--- src/api/gallery.ts
+++ src/api/gallery.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from '@/constant';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export type TGallery = {
-  _id: number;
+  _id: string;
   src: string;
   width: number;
   height: number;
@@ -16,13 +16,19 @@ type TGalleryResponse = {
 
 const gallery = createApi({
   reducerPath: 'gallery',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' }),
   endpoints: (builder) => ({
     gallery: builder.query<TGalleryResponse, void>({
       query: () => '/gallery',
     }),
+    deleteGallery: builder.mutation({
+      query: (id: string) => ({
+        url: `/gallery/${id}`,
+        method: 'DELETE',
+      }),
+    }),
   }),
 });
 
-export const { useGalleryQuery } = gallery;
+export const { useGalleryQuery, useDeleteGalleryMutation } = gallery;
 export { gallery };
